1d35ae2f75851aa793aa701ceb0819d4b00e7110
lincontroy <lincolnmunene37@gmail.com>
2025-12-10 18:07:35 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('user.dashboard');
+})->middleware(['auth'])->name('user.dashboard');
 Route::get('/checker', [PaymentController::class, 'checker']);
 
 Route::middleware(['auth', 'role:admin'])->group(function () {
